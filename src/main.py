229d0f4c3309c2a41b7a7b44181e3aa12c741b41
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-10-09 09:54:49 +0900

    :construction: model

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from sanic.response import json
 
 app = Sanic()
 
-@app.route("/")
+@app.route('/')
 async def index(request):
     return json({})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+@app.route('/learn', methods=['POST'])
+async def learn(request):
+    return json({})
+
+@app.route('/predict',  methods=['POST'])
+async def predict(request):
+    return json({})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
---
+++ src/models/item.py
@@ -0,0 +1,10 @@
+from typing import NamedTuple
+
+
+class Item(NamedTuple):
+    x1: int
+    x2: int
+    x3: int
+    x4: int
+    x5: int
+    y: int
