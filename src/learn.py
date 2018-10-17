081134af8fa9e2e65e541ab8af58d59b06c8d7ca
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-10-17 09:53:44 +0900

    :construction: learning

--- src/learn.py
+++ src/learn.py
@@ -1,7 +1,12 @@
+import os
+
 from keras.models import Sequential, Model
 
 
-def ItemModel():
+MODEL_FILE_PATH = 'model.h5'
+
+
+class ItemNet():
     model = Sequential()
     model.add(Dense(5,  input_shape=(5,), use_bias=True, activation='sigmoid'))
     model.add(Dropout(0.1))
@@ -11,3 +16,18 @@ def ItemModel():
     return model
 
 
+model = ItemNet()
+
+model.compile(loss='mean_squared_error',
+    optimizer=RMSprop(),
+    metrics=['accuracy'])
+
+if os.path.exists(MODEL_FILE_PATH):
+    model.load(MODEL_FILE_PATH)
+
+
+def predict():
+    pass
+
+def learn():
+    pass
